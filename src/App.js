b21a0f5a626c45e4009a13f61cd737e7ad8ae6f4
Bin <xuebinb@gmail.com>
2023-12-10 19:12:59 -0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+const count = 100;
+
+const list = [
+  { id: 1001, name: "Vue" },
+  { id: 1002, name: "React" },
+  { id: 1003, name: "Angular" },
+];
+
+const isLogin = true;
+
+const articleType = 3;
+
+function getArticleTem() {
+  if (articleType === 0) {
+    return <div>no pic</div>;
+  } else if (articleType === 1) {
+    return <div>one pic</div>;
+  } else {
+    return <div>three pic</div>;
+  }
+}
+
+function getName() {
+  return "jack";
+}
 
 function App() {
+  // const handleClick = () => {
+  //   console.log("button clicked");
+  // };
+
+  // const handleClick = (e) => {
+  //   console.log("button clicked", e);
+  // };
+
+  // const handleClick = (name) => {
+  //   console.log("button clicked", name);
+  // };
+
+  const handleClick = (name, e) => {
+    console.log("button clicked", name, e);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is App
+      {"this is message"}
+      {count}
+      {getName()}
+      {new Date().getDate()}
+      <div style={{ color: "red" }}>this is div</div>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+      {isLogin && <span>this is span</span>}
+      {isLogin ? <span>jack</span> : <span>loading...</span>}
+      {getArticleTem()}
+      <button onClick={(e) => handleClick('jack', e)}>click me</button>
     </div>
   );
 }
